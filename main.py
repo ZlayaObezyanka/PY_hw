006fcc0de5a439634d6a08cf10ad91323a6fc994
ZlayaObezyanka <den.abk44@gmail.com>
2023-08-28 20:26:46 +0300

    first job

---
+++ main.py
@@ -0,0 +1,18 @@
+import array
+import random
+
+# n = 0
+# coin = 0
+# n = int(input())
+
+# arr = array.array('i')
+
+# arr = [random.randint(0, 1) for i in range(n)]
+
+# for i in arr: 
+#     if(i == 0):
+#         coin += 1
+
+
+# print(arr)
+# print("требуется ", coin,  "перевертышей")
